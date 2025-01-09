4f6fafcf08e3212f18087a6fa876f8393f6d1359
Minach2002 <fathimathulmina2002@gmail.com>
2025-01-09 20:10:05 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var { engine } = require('express-handlebars'); // Correctly import 'engine'
+
+var app = express();
+
+// View engine setup
+app.engine('hbs', engine({
+  extname: 'hbs',
+  defaultLayout: 'layout',
+  layoutsDir: __dirname + '/views/layout',
+  partialsDir: __dirname + '/views/partials/'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+  // Set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  let products=[
+    {
+    name:"IPHONE 11",
+    category:"Mobile",
+    decription:"This is a good phone",
+    image:"https://th.bing.com/th/id/OIP.tIwbx1U9tqg-w0jPpfEnFgHaHa?w=176&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+    },
+    {
+      name:"One Plus 7T",
+      category:"Mobile",
+      decription:"This is a good phone",
+      image:"https://www.gizmochina.com/wp-content/uploads/2019/08/OnePlus-7T-c.jpg"
+    },
+    {
+      name:"OPPO 10x",
+      category:"Mobile",
+      decription:"This is a good phone",
+      image:"https://tse1.mm.bing.net/th?id=OIP.8pYAtgdR04LF2udm1iW_-AHaFj&rs=1&pid=ImgDetMain"
+    },
+    {
+      name:"MI Note 9 pro",
+      category:"Mobile",
+      decription:"This is a good phone",
+      image:"https://th.bing.com/th/id/OIP.L2onoIneQ2SwariDBkmplAHaHa?w=194&h=194&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+    }
+  ]
+  res.render('index', { products ,admin:false});
+});
+
+module.exports = router;
